96cb2d82e254896eab0b1006b950c7e15e56a8e4
samber <samber@users.noreply.github.com>
2024-12-24 10:38:17 +0000

    bump v2.5.1

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.21
 require (
 	github.com/fluent/fluent-logger-golang v1.9.0
 	github.com/samber/slog-common v0.18.0
-	go.uber.org/goleak v1.3.0
 )
 
 require (
--- version.go
+++ version.go
@@ -1,4 +1,4 @@
 package slogfluentd
 
 const name = "samber/slog-fluentd"
-const version = "VERSION" // replaced by .github/workflows/release.yml
+const version = "v2.5.1" // replaced by .github/workflows/release.yml
